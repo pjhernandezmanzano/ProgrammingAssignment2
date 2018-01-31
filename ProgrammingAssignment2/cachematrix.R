6f227ccae54bfab0f24f412e57a48650bf0267a3
Pedro Jose Hernandez Manzano <pj.hernandez.manzano@gmail.com>
2018-01-31 18:49:38 +0100

    v0

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -3,26 +3,13 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function (x = matrix()){
-  m <- NULL
-  get <- function () x
-  setinverse <- function(inverse) m <<-inverse
-  getinverse <- function() m
-  list(get = get, setinverse = setinverse,  getinverse = getinverse)
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data)
-  x$setinverse(m)
-  m
+        ## Return a matrix that is the inverse of 'x'
 }
-
